be50567774c0bf6954cceb2c07a54fe58fe5eb03
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-04-26 15:41:57 -0400

    Fixed keypad support to work with XO keypad - fixd line 16

--- Player.py
+++ Player.py
@@ -13,7 +13,7 @@ class Player:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT or event.key == pygame.K_KP4:
                 self.x -= self.speed
-            elif event.key == pygame.K_RIGHT or event.key == pygam.K_KP6:
+            elif event.key == pygame.K_RIGHT or event.key == pygame.K_KP6:
                 self.x += self.speed
             if event.key == pygame.K_UP or event.key == pygame.K_KP8:
                 self.y -= self.speed
