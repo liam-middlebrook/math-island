7d29b6673e581458176c7fdf73545b00806a4f94
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-04-26 15:41:19 -0400

    Fixed keypad support to work with XO keypad

--- Player.py
+++ Player.py
@@ -11,11 +11,11 @@ class Player:
         pygame.draw.rect(surface, (255, 0, 0), (self.x, self.y, 64, 64))
     def update(self, event):
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT or event.key == pygame.KP_Left:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_KP4:
                 self.x -= self.speed
-            elif event.key == pygame.K_RIGHT or event.key == pygam.KP_Right:
+            elif event.key == pygame.K_RIGHT or event.key == pygam.K_KP6:
                 self.x += self.speed
-            if event.key == pygame.K_UP or event.key == pygame.KP_Up:
+            if event.key == pygame.K_UP or event.key == pygame.K_KP8:
                 self.y -= self.speed
-            elif event.key == pygame.K_DOWN or event.key == pygame.KP_Down:
+            elif event.key == pygame.K_DOWN or event.key == pygame.K_KP2:
                 self.y += self.speed
