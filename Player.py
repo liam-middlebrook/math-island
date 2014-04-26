d938d303ff191df8102854bfa1e0a63060a2dbed
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-04-26 19:00:01 -0400

    Added player texture

--- Player.py
+++ Player.py
@@ -4,18 +4,18 @@ import pygame
 
 class Player:
     def __init__(self):
-        self.x = 128
-        self.y = 128
         self.speed = 64
+        self.texture = pygame.image.load("player.png")
+        self.rect = (128, 128, 64, 64)
     def draw(self, surface):
-        pygame.draw.rect(surface, (255, 0, 0), (self.x, self.y, 64, 64))
+        surface.blit(self.texture, self.rect)
     def update(self, event):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT or event.key == pygame.K_KP4:
-                self.x -= self.speed
+                self.rect.x -= self.speed
             elif event.key == pygame.K_RIGHT or event.key == pygame.K_KP6:
-                self.x += self.speed
+                self.rect.x += self.speed
             if event.key == pygame.K_UP or event.key == pygame.K_KP8:
-                self.y -= self.speed
+                self.rect.y -= self.speed
             elif event.key == pygame.K_DOWN or event.key == pygame.K_KP2:
-                self.y += self.speed
+                self.rect.y += self.speed
