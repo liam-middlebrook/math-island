3e6e0df14349213d9bfdb3345ddc53bdb221df0a
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-04-26 15:38:00 -0400

    Added keypad support to player - Fixed style for KP_Key

--- Player.py
+++ Player.py
@@ -11,11 +11,11 @@ class Player:
         pygame.draw.rect(surface, (255, 0, 0), (self.x, self.y, 64, 64))
     def update(self, event):
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT or event.key == pygame.KP_LEFT:
+            if event.key == pygame.K_LEFT or event.key == pygame.KP_Left:
                 self.x -= self.speed
-            elif event.key == pygame.K_RIGHT or event.key == pygam.KP_RIGHT:
+            elif event.key == pygame.K_RIGHT or event.key == pygam.KP_Right:
                 self.x += self.speed
-            if event.key == pygame.K_UP or event.key == pygame.KP_UP:
+            if event.key == pygame.K_UP or event.key == pygame.KP_Up:
                 self.y -= self.speed
-            elif event.key == pygame.K_DOWN or event.key == pygame.KP_DOWN:
+            elif event.key == pygame.K_DOWN or event.key == pygame.KP_Down:
                 self.y += self.speed
