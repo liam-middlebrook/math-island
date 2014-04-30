f6c64d44744a744aef9b5457c2ebc8ccd6cbf48f
Matt Soucy <msoucy@csh.rit.edu>
2014-04-30 17:57:13 -0400

    Support for random obstacle choice

--- level.py
+++ level.py
@@ -1,46 +1,45 @@
+import os
 import os.path as path
+import random
 from fractions import Fraction as Frac
 from collections import namedtuple
 
-#CONTENTHOME = 'content'
-CONTENTHOME = ''
+CONTENTHOME = 'content'
 LEVELHOME = path.join(CONTENTHOME, "levels")
+TILESETTINGS = path.join(LEVELHOME, "tiles")
+TILEHOME = path.join(CONTENTHOME, "tiles")
+OBSHOME = path.join(CONTENTHOME, "obstacles")
+WATERTILE = path.join(OBSHOME, "water.png")
 
 Coord = namedtuple('Coord', ['x', 'y'])
+
+obstacles = [path.join(OBSHOME, x) for x in os.listdir(OBSHOME)]
 
 def safeloadlines(fn):
     with open(fn) as f:
         return f.readlines()
 
-def loadimage(fn):
-    '''
-    Load an image
-
-    For now, stubbed out to just give the filename
-    '''
-    return fn
-
 class Tile(object):
     def __init__(self, name, image, frac):
         self.name = name
-        self.image = loadimage(image)
+        self.image = image
         self.passable = True
         self.cost = Frac(frac)
 
 class BlockingTile(object):
     def __init__(self, fn=None):
-        self.image = loadimage(fn)
+        self.image = fn or random.choice(obstacles)
         self.passable = False
 
 class WaterTile(BlockingTile):
     def __init__(self):
-        BlockingTile.__init__(self, path.join(CONTENTHOME, "water.png"))
+        BlockingTile.__init__(self, WATERTILE)
 
 class Tiles(object):
     def __init__(self):
-        assert path.exists(path.join(LEVELHOME, "tiles")), "Could not load tiles"
+        assert path.exists(TILESETTINGS), "Could not load tiles"
         self.tilemap = {}
-        with open(path.join(LEVELHOME, "tiles")) as tiledata:
+        with open(TILESETTINGS) as tiledata:
             for line in tiledata:
                 sym, name, image, frac = line.split("\t")
                 self.tilemap[sym] = Tile(name, image, frac)
@@ -65,13 +64,12 @@ class Level(object):
                      for line in data if line.strip() and not line.startswith("#")]
         self.height = len(self._map)
         self.width = len(self._map[0])
-        print(self._map)
     def __getitem__(self, indices):
         assert len(indices) == 2, "Invalid level lookup"
         return self._map[indices[0]][indices[1]]
 
 if __name__ == '__main__':
-    lv = Level("levels/001.ilv")
+    lv = Level(path.join(LEVELHOME, "001.ilv"))
     print "TITLE:", lv.title
     print "TEXT:\n",lv.text
     for i in range(lv.width):
