93565c6de13bb1843b0cf700ab33c0e4db24a100
Matt Soucy <msoucy@csh.rit.edu>
2014-05-01 22:20:00 -0400

    Added fuel system and made [] consistent

--- level.py
+++ level.py
@@ -19,6 +19,9 @@ def safeloadlines(fn):
     with open(fn) as f:
         return f.readlines()
 
+def bound(low, val, hi):
+    return max(low, min(val, hi))
+
 class Tile(object):
     def __init__(self, name, image, frac):
         self.name = name
@@ -65,14 +68,17 @@ class Level(object):
                      for line in data if line.strip() and not line.startswith("#")]
         self.height = len(self._map)
         self.width = len(self._map[0])
+        self.fuel = {Coord(int(x), int(y)): Frac(f)
+                     for x, y, f in [
+                         line.split() for line in take("#Refuel")]}
     def __getitem__(self, indices):
         assert len(indices) == 2, "Invalid level lookup"
-        return self._map[indices[0]][indices[1]]
+        return self._map[indices[1]][indices[0]]
+
+    def getcost(self, x, y):
+        return bound(0, self[x,y].cost + self.fuel.pop(Coord(x,y), 0), 1)
 
 if __name__ == '__main__':
-    lv = Level(path.join(LEVELHOME, "001.ilv"))
-    print "TITLE:", lv.title
-    print "TEXT:\n",lv.text
-    for i in range(lv.width):
-        for j in range(lv.height):
-            print lv[i,j]
+    lv = Level(path.join(LEVELHOME, "002.ilv"))
+    print lv.getcost(4,3)
+    print lv.getcost(4,3)
