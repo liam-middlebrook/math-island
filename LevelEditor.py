f473b3e3460afc54f5ee4b6781abeca72498a5f5
Matt Soucy <msoucy@csh.rit.edu>
2014-05-13 15:40:03 -0400

    Save/Load title/text

--- LevelEditor.py
+++ LevelEditor.py
@@ -58,11 +58,11 @@ class Editor(Frame):
         infoframe = Frame(self.master)
         infoframe.pack(side=TOP)
         Label(infoframe, text="Title: ").pack(side=TOP)
-        self.titlebar = Entry(infoframe)
-        self.titlebar.pack()
+        self.title = StringVar()
+        Entry(infoframe, textvariable=self.title).pack()
         Label(infoframe, text="Text: ").pack(side=TOP)
-        self.textbox = Text(infoframe, width=60, height=4)
-        self.textbox.pack()
+        self.text = Text(infoframe, width=60, height=4)
+        self.text.pack()
 
         fuelframe = Frame(self.master)
         fuelframe.pack(side=BOTTOM)
@@ -74,13 +74,13 @@ class Editor(Frame):
 
         tileframe = Frame(self.master)
         tileframe.pack(side=RIGHT)
-        def _atb(char, i):
+        getcost = lambda t: getattr(Tiles[t], "cost", None)
+        tiles = sorted(Tiles, key=getcost)
+        for i, c in enumerate(itertools.chain(('.','w'), tiles)):
             btn = Button(tileframe,
-                         text="{0}: {1}".format(char, getattr(Tiles[char], "cost", None)),
-                         command=lambda c=char: self.setstate(TileChangeState(c)))
+                         text="{0}: {1}".format(c, getcost(c)),
+                         command=lambda c=c: self.setstate(TileChangeState(c)))
             btn.grid(column=(i//10), row=(i%10), sticky=N+E+S+W)
-        for i, c in enumerate(itertools.chain(('.','w'), Tiles)):
-            _atb(c, i)
 
         mapframe = Frame(self.master)
         mapframe.pack(side=LEFT)
@@ -94,6 +94,10 @@ class Editor(Frame):
                 btn.grid(row=y, column=x, sticky=N+E+S+W)
 
         self.redraw()
+
+        '''
+        TODO: START/END/STARTFUEL BUTTONS
+        '''
 
     def redraw(self):
 
@@ -113,6 +117,8 @@ class Editor(Frame):
             self.redraw()
 
     def save(self):
+        self.level.title = self.title.get().strip()
+        self.level.text = self.text.get(1.0, END).strip()
         fn = filedialog.asksaveasfilename(**self.file_opt)
         if not fn: return
         with open(fn, "w") as fi:
@@ -122,6 +128,9 @@ class Editor(Frame):
         fn = filedialog.askopenfilename(**self.file_opt)
         if fn:
             self.level = Level(fn)
+        self.title.set(self.level.title)
+        self.text.delete(1.0, END)
+        self.text.insert(1.0, self.level.text)
         self.redraw()
 
 def main():
